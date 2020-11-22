711b1593fb6c954cd45b8e74e5ec771a3abb4464
hosam mohamed <hosamelmalt@gmail.com>
2020-11-22 15:43:34 -0800

    Initial Commit

---
+++ Simple App Store/Simple_App_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_App_StoreApp.swift
+//  Simple App Store
+//
+//  Created by Hossam on 11/22/20.
+//
+
+import SwiftUI
+
+@main
+struct Simple_App_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
